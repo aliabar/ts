1e4600167b6a82b66e41b9ae189de457a01a866e
aliabar <114748399+aliabar@users.noreply.github.com>
2022-12-20 03:38:36 -0800

    first commit

---
+++ sj.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("nav").style.width="250px";
+    document.getElementById("openNav").style.display="none";
+}
+
+function closeNav(){
+    document.getElementById("nav").style.width="0px";
+    document.getElementById("openNav").style.display="block";
+
+}
